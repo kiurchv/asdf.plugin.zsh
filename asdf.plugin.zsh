5fb51e9099230de602821e3cae5836162be1b768
Myroslav Kiurchev <kiurchv@gmail.com>
2019-01-16 21:56:33 +0200

    Revert "Update path to completion script"

--- asdf.plugin.zsh
+++ asdf.plugin.zsh
@@ -2,5 +2,5 @@ asdf_dir="${asdf_dir:-$HOME/.asdf}"
 
 if [[ -d $asdf_dir ]]; then
   source $asdf_dir/asdf.sh
-  source $asdf_dir/etc/bash_completion.d/asdf.bash
+  source $asdf_dir/completions/asdf.bash
 fi
